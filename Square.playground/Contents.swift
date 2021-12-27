7b62cece9a477dedb5d750684ded02fa4cad231a
ybnr1130 <muuckk@gmail.com>
2021-12-27 14:18:01 +0900

    Delete let result to change simple code

--- Square.playground/Contents.swift
+++ Square.playground/Contents.swift
@@ -1,8 +1,9 @@
 struct Square {
     var sideLength = 3  //辺の長さ
     var area:Int {      //面積
-        let result = sideLength * sideLength
-        return result
+//定数にしなくとも記述可能（下記参考）
+//        let result = sideLength * sideLength
+        return sideLength * sideLength
     }
 }
 let square = Square()  //Square構造体からインスタンスを作る
